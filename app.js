ea4a3cea9a22640c7a6f3bac00db93efcfc51118
jeffmlb91 <jeffmlb91@gmail.com>
2021-10-21 23:19:19 -0400

    add app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+//const inquirer = require('inquirer');
+
+/*const profileDataArgs = process.argv.slice(2, process.argv.lenght);
+console.log(profileDataArgs);
+
+const animalArray = ['dog', 'cat', 'pig'];
+
+animalArray.push('cow');
+
+const personObj = {
+    name: 'Geoffrey',
+    age: 26
+};
+personObj.age = 100;
+personObj.occupation = 'Developer';
+*/
+
+const printProfileData = profileDataArr => {
+    for (let i = 0; i <profileDataArr.length; i++) {
+        console.log(profileDataA[i]);
+    } 
+};
