9e7486f10d420012d801acd7c268e4208d7963dc
jeffmlb91 <jeffmlb91@gmail.com>
2021-10-27 01:36:38 -0400

    refactor app.js and add inquirer and node modules

--- app.js
+++ app.js
@@ -1,4 +1,43 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What us your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+
+/*const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('index.html', pageHTML, err => {
+    if (err) throw err;
+
+    console.log('Portfolio completed! Check out index.html to see the output');
+});*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const fs = require('fs');
 const generatePage = require('./src/page-template');
 
 const profileDataArgs = process.argv.slice(2);
@@ -11,7 +50,7 @@ fs.writeFile('index.html', generatePage(name, github), err => {
     if (err) throw new Error(err);
 
     console.log('Portfolio completed! Check out index.html to see the output');
-});
+});*/
 
 
 
