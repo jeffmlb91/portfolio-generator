380096c81638ea4aa9d7699ada5532e7bfc044cf
jeffmlb91 <jeffmlb91@gmail.com>
2021-10-27 00:13:38 -0400

    add app.js logic and page-template.js

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+//const name = profileDataArgs[0];
+//const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
+
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio completed! Check out index.html to see the output');
+});
+
+
+
+
+
+
+
+
+
 //const inquirer = require('inquirer');
 
 /*const profileDataArgs = process.argv.slice(2, process.argv.lenght);
@@ -63,36 +86,4 @@ console.log(generatePage(name, github));
 */
 
 // this is a allow us to return multiline input
-
-const profileDataArgs = process.argv.slice(2, process.argv.lenght);
-const fs = require('fs');
-
-//const name = profileDataArgs[0];
-//const github = profileDataArgs[1];
-const [name, github] = profileDataArgs;
-
-const generatePage = ( userName, githubName) => {
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-        <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0>
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Portfolio Demo</title>
-        </head>
-
-        <body>
-            <h1>${name}</h1>
-            <h2><a href="https://github.com/${github}">Github</a></h2>
-        </body>
-    </html>  
-    `
-};
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-
-    console.log('Portfolio completed! Check out index.html to see the output');
-});
 
